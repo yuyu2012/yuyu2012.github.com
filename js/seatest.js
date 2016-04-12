fd5416701310f9bd3f48aabf967ca1b9e6ab4028
yuyu2012 <601971198@qq.com>
2016-04-12 15:10:22 +0800

    seajs

--- js/seatest.js
+++ js/seatest.js
@@ -1,2 +1,13 @@
 // JavaScript Document
+
+
+define(function(require, exports, module){
+		   //引用jQuery模块
+           require('jquery');
+		   
+		   $(document).ready(function(){
+
 alert("454");
+})
+		  
+        });
